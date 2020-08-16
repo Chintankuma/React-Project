0f9b088ce24fd44c497a7fe2832ed5f9a5d6d073
Chintankuma <chintanvaghani555@gmail.com>
2020-08-16 19:47:20 +0200

    hero component with added all banner

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export const Rooms = () => {
-  return <div>hello from room page</div>;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="Our rooms">
+        <Link to="/" className="btn-primary">
+          return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
