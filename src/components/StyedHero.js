ba0b5fffc46526f6e1f4f8dd3ada0189e01df262
Chintankuma <chintanvaghani555@gmail.com>
2020-08-18 13:03:13 +0200

    single room completed

---
+++ src/components/StyedHero.js
@@ -0,0 +1,15 @@
+//npm install --save styled-component
+//with that we can used javascript in css
+
+import styled from "styled-components";
+import defaultImg from "../images/room-1.jpeg";
+
+const StyledHero = styled.header`
+  min-height: 60vh;
+  background: url(${(props) => props.img}) center/cover no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default StyledHero;
